80efcfdb9ed6eaebfd5a9c955247497e80715f3b
bwheel17 <wheelerwhiz@gmail.com>
2019-05-05 11:40:50 -0700

    Merge branch 'master' of https://github.com/Aeon-ic/A-Pilots-Traumatic-Slimy-Gooey-Extravagnza
    # Conflicts:
    #	Assets/PlayMaker/Editor/Resources/EditorStartupPrefs.asset

---
+++ Assets/Scripts/PickupHealth.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PickupHealth : MonoBehaviour
+{
+    [Tooltip("This is the amount of health that is restored to the player on pickup")]
+    public int healthRestored = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Player>().AddHealth(healthRestored);
+            Destroy(this.gameObject);
+        }
+    }
+}
