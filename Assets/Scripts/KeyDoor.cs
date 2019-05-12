cb702c953d25ef7cbbe6e50d91426ebc30efe2bc
Carter Floyd <craterf8@gmail.com>
2019-05-12 16:20:25 -0500

    Requested Key Door script

---
+++ Assets/Scripts/KeyDoor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class KeyDoor : MonoBehaviour
+{
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("Player"))
+    {
+      if (Player.instance.hasKeycard)
+      {
+        Destroy(this.gameObject);
+      }
+    }
+  }
+}
