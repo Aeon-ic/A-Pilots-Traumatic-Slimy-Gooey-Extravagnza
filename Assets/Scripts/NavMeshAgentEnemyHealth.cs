f306604edee16c7946286bbaebede05914bf490b
Carter Floyd <craterf8@gmail.com>
2019-05-12 16:51:19 -0500

    Random Sounds on EnemyHealth script

--- Assets/Scripts/NavMeshAgentEnemyHealth.cs
+++ Assets/Scripts/NavMeshAgentEnemyHealth.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class NavMeshAgentEnemyHealth : MonoBehaviour, IEnemyHealth
 {
   [Tooltip("This is a float that starts at the maximum enemy health and then shows current health")]
   public float enemyHealth;
+  [Tooltip("This is the audio clip that is played randomly while the enemy is alive")]
+  public AudioClip soundEffect;
+  [Tooltip("This is a float that defines the minimum amount of time between sound effects")]
+  public float minSoundEffectTime = 1f;
+  [Tooltip("This is a float that defines the maximum amount of time between sound effects")]
+  public float maxSoundEffectTime = 10f;
+  AudioSource audioSource;
+
+  private void Start()
+  {
+    audioSource = this.gameObject.GetComponent<AudioSource>();
+    StartCoroutine(RandomSounds());
+  }
 
   public void TakeDamage(float damage)
   {
@@ -16,6 +31,16 @@ public class NavMeshAgentEnemyHealth : MonoBehaviour, IEnemyHealth
 
   public void Die()
   {
+    StopAllCoroutines();
     Destroy(this.gameObject);
+  }
+
+  IEnumerator RandomSounds()
+  {
+    while (true)
+    {
+      audioSource.PlayOneShot(soundEffect);
+      yield return new WaitForSecondsRealtime(Random.Range(minSoundEffectTime, maxSoundEffectTime));
+    }
   }
 }
