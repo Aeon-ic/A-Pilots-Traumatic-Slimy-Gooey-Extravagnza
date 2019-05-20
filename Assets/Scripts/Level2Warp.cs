2e7be75a97cf002b90dfb81ac97fb38af130d6da
Carter Floyd <craterf8@gmail.com>
2019-05-20 18:07:34 -0500

    Water Physics in Level 1, Warp for Level 2 presentation

---
+++ Assets/Scripts/Level2Warp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Warp : MonoBehaviour
+{
+    public Transform warpPoint;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Player.instance.player.gameObject.transform.position = warpPoint.position;
+        }
+    }
+}
---
+++ Assets/Scripts/WaterPhysics.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPhysics : MonoBehaviour
+{
+    [Tooltip("This is the force that the water pushes the player up while in the water")]
+    public float waterForce = 20f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * waterForce);
+        }
+    }
+}
